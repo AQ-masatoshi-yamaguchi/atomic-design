b956a52d2598d45a4ff5fff84db1317d6207adfa
AQ-masatoshi-yamaguchi <69567949+AQ-masatoshi-yamaguchi@users.noreply.github.com>
2022-02-05 10:49:49 +0900

    organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "まさお",
+  image: "https://source.unsplash.com/OzAeZPNsLXk",
+  email: "test@example.com",
+  phone: "020-0000-1111",
+  company: {
+    name: "テスト会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput></SearchInput>
+      <UserCard user={user}></UserCard>
     </div>
   );
 }
